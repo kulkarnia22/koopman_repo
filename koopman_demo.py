8b9960d5c6ad92d8c9972f1843195d598da7d78e
Arjun <kulkarnia@uchicago.edu>
2024-06-05 15:16:54 -0500

    have data

--- koopman_demo.py
+++ koopman_demo.py
@@ -2,4 +2,9 @@ import pykoop
 import pydmd
 import pandas as pd
 import openpyxl
-pd.read_excel()
+
+excel_file = 'data/Ga_Test_001_2019_08_1508_15_19.xlsm'
+sheet_name = 'Processed data'
+data_frame = pd.read_excel(excel_file, sheet_name, usecols=['Outlet flow rate (liter/sec)'])
+print(type(data_frame))
+
