3c526d13995569d4a7c27268df2b0f79df3f4400
Arjun <kulkarnia@uchicago.edu>
2024-06-19 13:23:11 -0500

    made change

--- code/Fiber_data_analysis.py
+++ code/Fiber_data_analysis.py
@@ -8,7 +8,7 @@ tsv_file = 'data\LDRD_Test001-03_2019-04-24_15-23-17_ch3_full.tsv'
 data_frame = pd.read_csv(tsv_file, sep='\t', header = None, skiprows = 28, nrows = 100)
 temp_data = data_frame.to_numpy()
 new = []
-for lst in data:
+for lst in temp_data:
     new.append(lst[1:])
 temp_data = np.array(new)
 
